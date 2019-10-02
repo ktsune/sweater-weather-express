aa35c5ec2483daefddf1a9d826b53d8f76b50ccb
Sarah Tatro <statrox4@gmail.com>
2019-10-02 15:36:41 -0600

    Change password to passwordDigest to match db attribute on user

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -4,33 +4,31 @@ var bcrypt = require('bcrypt')
 var saltRounds = 10;
 var uuid = require('uuidv4').default;
 var User = require('../../../models').User
+var pry = require('pryjs')
 
 /* POST account creation */
 router.post("/", function(req, res, next) {
   res.setHeader("Content-Type", "application/json");
-  bcrypt.hash(req.body.passwordDigest, saltRounds, function (err, hash) {
+  if (req.body.password == req.body.passwordConfirmation) {
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
 
-  if (req.body.passwordDigest == req.body.passwordConfirmation) {
     User.create({
         email: req.body.email,
-        password: hash,
+        passwordDigest: hash,
         apiKey: uuid()
       })
       .then(user => {
         res.status(201).send(JSON.stringify({
-          apiKey: user.apiKey,
-          status: res.status
+          apiKey: user.apiKey
           })
         );
       })
       .catch(error => {
         res.status(422).send({ error: "Please choose a new email" });
       });
-  } else {
+  })} else {
     res.status(401).send({ error: "Passwords do not match" });
-  }
-
-  });
+  };
 });
 
 module.exports = router;
