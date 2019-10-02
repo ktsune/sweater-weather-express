cdb49023771772eb92f9ff04b3d7cef7acc17826
Sarah Tatro <statrox4@gmail.com>
2019-10-01 18:20:02 -0600

    Create user model

---
+++ models/user.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    email: DataTypes.STRING,
+    apiKey: DataTypes.STRING,
+    passwordDigest: DataTypes.STRING
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
