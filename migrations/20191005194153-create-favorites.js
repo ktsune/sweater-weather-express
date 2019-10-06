16147211bbe0427fef555e8fe9527ef02690dcbe
Sarah Tatro <statrox4@gmail.com>
2019-10-06 11:33:43 -0600

    Create migration for favorites table and one for adding a foreign key user id to favorites

---
+++ migrations/20191005194153-create-favorites.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Favorites', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      location: {
+        type: Sequelize.STRING
+      },
+      // userId: {
+      //   type: Sequelize.INTEGER,
+      //   // allowNull: false,
+      //   references: {
+      //     model: 'Users',
+      //     key: 'id'
+      //   }
+      // },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Favorites');
+  }
+};
